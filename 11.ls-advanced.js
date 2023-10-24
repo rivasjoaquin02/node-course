277eebed5ba2dc4cedee641720e5a42050a195b9
strange <rivasjoaquin02@gmail.com>
2023-10-24 17:45:45 -0400

    backup

--- 11.ls-advanced.js
+++ 11.ls-advanced.js
@@ -3,7 +3,7 @@ const path = require("node:path");
 
 const folder = process.argv[2] ?? ".";
 
-async function ls(folder: string) {
+async function ls(folder) {
 	let files;
 
 	try {
@@ -18,18 +18,18 @@ async function ls(folder: string) {
 		let stats;
 
 		try {
-			stats = fs.stat(filePath); // information
+			stats = await fs.stat(filePath); // information
 		} catch (error) {
 			console.error(`File ${filePath} could not be read`);
 			process.exit(1);
 		}
 
 		const isDirectory = stats.isDirectory();
-		const fileType = isDirectory ? "d" : "-";
+		const fileType = isDirectory ? "d" : "f";
 		const fileSize = stats.size;
 		const fileModified = stats.mtime.toLocaleString();
 
-		return `${fileType} ${file} ${fileSize.toLocaleString()} ${fileModified}`;
+		return `${fileType} ${file.padEnd(25)} ${fileSize.toLocaleString().padStart(10)} ${fileModified}`;
 	});
 
 	const filesInfo = await Promise.all(filesPromises);
