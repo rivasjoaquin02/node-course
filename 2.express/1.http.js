1e5e47da719da4b4ed98fbd9c0ffce5bee480033
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2023-10-28 14:08:12 -0400

    backup

--- 2.express/1.http.js
+++ 2.express/1.http.js
@@ -1,4 +1,5 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const desiredPort = process.env.PORT ?? 3000;
 
@@ -8,6 +9,16 @@ const server = http.createServer((req, res) => {
 	if (req.url === "/") {
 		res.statusCode = 200;
 		res.end("<h1>Hello World</h1>");
+	} else if (req.url === "/img.jpg") {
+		fs.readFile("./avatar.jpg", (err, data) => {
+			if (err) {
+				res.statusCode = 500;
+				res.end("<h1>500 Internal Server Error </h1>");
+			} else {
+				res.setHeader("Content-Type", "image/jpg");
+				res.end(data);
+			}
+		});
 	} else if (req.url == "/contact") {
 		res.statusCode = 200;
 		res.end("<h1>Contact</h1>");
---
+++ 2.express/3.express.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const dittoJSON = require("./pokemon/ditto.json");
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+app.disable("x-powered-by");
+
+// middleware
+// app.use((req, res, next) => {
+// 	// ej: track the request to DB
+// 	// ej: check if the users has cookies
+
+// 	if (req.method !== "POST") return next();
+// 	if (req.headers["content-type"] !== "application/json") return next();
+
+// 	let body = "";
+// 	req.on("data", (chunk) => {
+// 		body += chunk.toString();
+// 	});
+// 	req.on("end", () => {
+// 		const data = JSON.parse(body);
+// 		data.timestamp = Date.now();
+// 		// mutate the req and put the info in the req.body
+// 		req.body = data;
+// 		next();
+// 	});
+// });
+
+// the same as the commented
+app.use(express.json())
+
+app.get("/", (req, res) => {
+	// res.send("<h1>My page</h1>");
+	res.json({ message: "Hello World" });
+});
+
+app.get("/pokemon/ditto", (req, res) => {
+	res.json(dittoJSON);
+});
+
+app.post("/pokemon", (req, res) => {
+    // req.body should be stored in DB
+	res.status(201).json(req.body);
+});
+
+app.use((req, res) => {
+	res.status(404).send("<h1>404</h1>");
+});
+
+app.listen(PORT, () => {
+	console.log(`server listening on port http://localhost:${PORT}`);
+});
